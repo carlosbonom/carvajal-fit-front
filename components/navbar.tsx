67f291684116b2f4cb7d4d884749e3de884462b5
carlosbonom <bonomellicarlos@gmail.com>
2025-11-11 01:29:53 -0300

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -80,7 +80,7 @@ export const Navbar = () => {
         <NavbarMenuToggle className="text-white" />
       </NavbarContent>
 
-      <NavbarMenu>
+      <NavbarMenu className="bg-transparent-900">
         {/* {searchInput} */}
         {/* <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
