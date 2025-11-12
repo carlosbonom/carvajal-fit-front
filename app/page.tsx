9c1015989a723e0f7f0aff57f489396bf0988eeb
carlosbonom <bonomellicarlos@gmail.com>
2025-11-11 22:51:43 -0300

    git push

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { HeroSection } from "@/components/hero-section";
 import { SuccessStories } from "@/components/Success-stories";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <HeroSection />
       <SuccessStories />
+      <Footer />
     </>
   );
 }
