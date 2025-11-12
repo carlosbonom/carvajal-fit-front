9c95eee5136ae2b6665a1f078478da7f4b2528c0
carlosbonom <bonomellicarlos@gmail.com>
2025-11-12 01:57:04 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <HeroSection />
       <SuccessStories />
-      {/* <MembershipCard /> */}
+      <MembershipCard />
       <MembershipCardv2 />
       <Teachers />
       <Footer />
